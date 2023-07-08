cba21a6d59b92aed2ddb7e3631deeef058833176
Ashutosh Kumawat <aurecordsofficial@gmail.com>
2023-07-08 14:41:31 +0530

    render now

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-const jsonServer = require("json-server");
+const jsonServer = require("json-server"); // importing json-server library
 const server = jsonServer.create();
 const router = jsonServer.router("address.json");
 const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 8080; //  chose port from here like 8080, 3001
 
 server.use(middlewares);
-server.use("", router);
-server.listen(process.env.PORT || 5000, () => {
-  console.log("JSON Server is running");
-});
+server.use(router);
+
+server.listen(port);
